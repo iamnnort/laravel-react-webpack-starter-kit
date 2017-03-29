df26370ae58dadd69c2243af8bb0e4c2f3f6cb0d
nikitapavets <nikitapavets@gmail.com>
2017-03-30 01:32:44 +0300

    init

---
+++ frontend/src/client/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render = () =>
+        <div>
+            Client HomePage
+        </div>
+}
+
+export default HomePage;
